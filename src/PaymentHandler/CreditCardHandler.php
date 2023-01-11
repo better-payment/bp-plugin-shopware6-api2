90d257e8bb6daf69bf7dcc7dbbe1d356106b91ff
Hasan Hasanzade <h.hesen@hotmail.com>
2023-01-11 12:19:36 +0400

    added plugin configuration reader and order parameters reader helper classes

--- src/PaymentHandler/CreditCardHandler.php
+++ src/PaymentHandler/CreditCardHandler.php
@@ -2,6 +2,8 @@
 
 namespace BetterPayment\PaymentHandler;
 
+use BetterPayment\Util\ConfigReader;
+use BetterPayment\Util\OrderParametersReader;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Request as GuzzleRequest;
@@ -13,21 +15,26 @@ use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentProcessException;
 use Shopware\Core\Checkout\Payment\Exception\CustomerCanceledAsyncPaymentException;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class CreditCardHandler implements AsynchronousPaymentHandlerInterface
 {
     private OrderTransactionStateHandler $orderTransactionStateHandler;
-//    private SystemConfigService $systemConfigService;
+    private ConfigReader $configReader;
+    private OrderParametersReader $orderParametersReader;
     private Client $client;
 
-    public function __construct(OrderTransactionStateHandler $orderTransactionStateHandler/*, SystemConfigService $systemConfigService*/)
+    public function __construct(
+        OrderTransactionStateHandler $orderTransactionStateHandler,
+        ConfigReader $configReader,
+        OrderParametersReader $orderParametersReader
+    )
     {
         $this->orderTransactionStateHandler = $orderTransactionStateHandler;
-//        $this->systemConfigService = $systemConfigService;
-        $this->client = new Client();
+        $this->configReader = $configReader;
+        $this->orderParametersReader = $orderParametersReader;
+//        $this->client = new Client();
     }
 
     /**
@@ -35,14 +42,9 @@ class CreditCardHandler implements AsynchronousPaymentHandlerInterface
      */
     public function pay(AsyncPaymentTransactionStruct $transaction, RequestDataBag $dataBag, SalesChannelContext $salesChannelContext): RedirectResponse
     {
-//        dd($this->systemConfigService->get('BetterPayment.config.environment'));
         // Method that sends the return URL to the external gateway and gets a redirect URL back
         try {
-            // TODO remove below temporary line for url shortening
-            $this->client->post('https://lightfulweb.free.beeceptor.com/returnurl', [
-                'body' => $transaction->getReturnUrl()
-            ]);
-            $redirectUrl = $this->sendReturnUrlToExternalGateway($transaction->getReturnUrl());
+            $redirectUrl = $this->sendRequestToExternalGateway($transaction);
         } catch (\Exception $e) {
             throw new AsyncPaymentProcessException(
                 $transaction->getOrderTransaction()->getId(),
@@ -59,31 +61,28 @@ class CreditCardHandler implements AsynchronousPaymentHandlerInterface
      */
     public function finalize(AsyncPaymentTransactionStruct $transaction, Request $request, SalesChannelContext $salesChannelContext): void
     {
-        $transactionId = $transaction->getOrderTransaction()->getId();
+//        $transactionId = $transaction->getOrderTransaction()->getId();
 
         // Example check if the user cancelled. Might differ for each payment provider
-        if ($request->query->getAlpha('status') == 'canceled') {
-            throw new CustomerCanceledAsyncPaymentException(
-                $transactionId,
-                'Customer canceled the payment on the payment page'
-            );
-        }
+//        if ($request->query->getAlpha('status') == 'canceled') {
+//            throw new CustomerCanceledAsyncPaymentException(
+//                $transactionId,
+//                'Customer canceled the payment on the payment page'
+//            );
+//        }
 
-        // Example check for the actual status of the payment. Might differ for each payment provider
-        $paymentState = $request->query->getAlpha('status');
-
+        // When it returns to success url mark payment as paid
         $context = $salesChannelContext->getContext();
-        if ($paymentState === 'completed') {
-            // Payment completed, set transaction status to "paid"
-            $this->orderTransactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context);
-        } else {
-            // Payment not completed, set transaction status to "open"
-            $this->orderTransactionStateHandler->reopen($transaction->getOrderTransaction()->getId(), $context);
-        }
+        // Payment completed, set transaction status to "paid"
+        $this->orderTransactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context);
     }
 
-    private function sendReturnUrlToExternalGateway(string $getReturnUrl): string
+    private function sendRequestToExternalGateway(AsyncPaymentTransactionStruct $transaction): string
     {
+        $orderParameters = $this->orderParametersReader->getAllParameters($transaction);
+        dd($orderParameters);
+
+//        dd($transaction);
         // Do some API Call to your payment provider
         $headers = [
             'Authorization' => 'Basic NzBhYmQ1OTQwODQ3ODdhMzkyZTg6NGE2NmI5MWU5YjVjOTBjYTQ3YjA=',
@@ -92,19 +91,18 @@ class CreditCardHandler implements AsynchronousPaymentHandlerInterface
         $options = [
             'form_params' => [
                 'payment_type' => 'cc',
-                'amount' => '15.9',
                 'postback_url' => 'https://lightfulweb.free.beeceptor.com/bp-cc',
-                'success_url' => substr($getReturnUrl,0, 255), // TODO this needs to be less than 255 chars
-                'error_url' => substr($getReturnUrl,0, 255),
-                'country' => 'DE',
+                'success_url' => $transaction->getReturnUrl(),
+                'error_url' => 'http://localhost:8888/lightfulweb/shopware6/public/account/order/edit/'
+                    .$transaction->getOrder()->getId()
+                    .'?error-code=CHECKOUT__ASYNC_PAYMENT_PROCESS_INTERRUPTED', // TODO edit this temp url
             ]
         ];
 
-        $request = new GuzzleRequest('POST', 'https://devapi.betterpayment.de/rest/payment', $headers);
-//        $request = new GuzzleRequest('POST', 'https://lightfulweb.free.beeceptor.com/bp-cc', $headers);
+        $request = new GuzzleRequest('POST', $this->configReader->getAPIHostName().'/rest/payment', $headers);
         try {
             $response = $this->client->send($request, $options);
-            if ($response->getStatusCode() == 200) { // TODO almost all responses are 200 coded, so check and handle them accordingly
+            if ($response->getStatusCode() == 200) { // TODO almost all responses are 200 code, so check and handle them accordingly
                 return json_decode((string) $response->getBody())->action_data->url;
             }
             else {
