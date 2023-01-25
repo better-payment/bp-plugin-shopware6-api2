ad6becb6521ffc8788849e205c95876b340526a8
Hasan Hasanzade <h.hesen@hotmail.com>
2023-01-25 15:43:11 +0400

    stored better_payment_transaction_id as order transaction custom fields json property

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -10,6 +10,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WebhookController extends AbstractController
@@ -20,27 +21,16 @@ class WebhookController extends AbstractController
     public function __construct(
         OrderTransactionStateHandler $orderTransactionStateHandler,
         EntityRepositoryInterface $orderTransactionRepository
-    )
-    {
+    ){
         $this->orderTransactionStateHandler = $orderTransactionStateHandler;
         $this->orderTransactionRepository = $orderTransactionRepository;
     }
 
     /**
-     * @Route("/api/betterpayment/webhook", methods={"POST", "GET"}, defaults={"_routeScope"={"api"}, "auth_required"=false, "auth_enabled"=false}, name="api.betterpayment.webhook")
+     * @Route("/api/betterpayment/webhook", methods={"POST"}, defaults={"_routeScope"={"api"}, "auth_required"=false, "auth_enabled"=false}, name="api.betterpayment.webhook")
      */
-    public function handle(Request $request, Context $context)
+    public function handle(Request $request, Context $context): Response
     {
-        // request parameters example
-//        transaction_id=603cd960-5cb9-4947-9ab6-def2c190b499&
-//        status_code=3&
-//        status=completed&
-//        order_id=123000&
-//        message=Request%20successfully%20processed%20in%20%27Merchant%20in%20Connector%20Test%20Mode%27%20%28000.100.112%29&
-//        additional_transaction_data=&
-//        included_fees=0.00&
-//        checksum=0bd18b647c4d5caf65b71f37c40afb779bf1dadd
-
         // TODO verify by $request->get('checksum')
         if (true) {
             $betterPaymentTransactionID = $request->get('transaction_id');
@@ -49,17 +39,17 @@ class WebhookController extends AbstractController
 
             $this->updateOrderTransactionState($orderTransactionID, $betterPaymentTransactionState, $context);
 
-            // TODO log that webhook handled successfully with $request->get('message')
+            return new Response($request->get('message'), 200);
         }
         else {
-            // TODO log that checksum verification failed for $betterPaymentTransactionID
+            return new Response('checksum verification failed', 401);
         }
     }
 
     private function getOrderTransactionByBetterPaymentTransactionID(string $betterPaymentTransactionID, Context $context): OrderTransactionEntity
     {
         $criteria = new Criteria();
-        $criteria->addFilter(new EqualsFilter('customFields.betterPaymentTransactionID', $betterPaymentTransactionID));
+        $criteria->addFilter(new EqualsFilter('customFields.better_payment_transaction_id', $betterPaymentTransactionID));
 
         return $this->orderTransactionRepository->search($criteria, $context)->first();
     }
--- src/Util/BetterPaymentClient.php
+++ src/Util/BetterPaymentClient.php
@@ -5,17 +5,25 @@ namespace BetterPayment\Util;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
+use Shopware\Core\DevOps\Environment\EnvironmentHelper;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 
 class BetterPaymentClient
 {
     private ConfigReader $configReader;
     private OrderParametersReader $orderParametersReader;
+    private EntityRepositoryInterface $orderTransactionRepository;
     private Client $client;
 
-    public function __construct(ConfigReader $configReader, OrderParametersReader $orderParametersReader)
-    {
+    public function __construct(
+        ConfigReader $configReader,
+        OrderParametersReader $orderParametersReader,
+        EntityRepositoryInterface $orderTransactionRepository
+    ){
         $this->configReader = $configReader;
         $this->orderParametersReader = $orderParametersReader;
+        $this->orderTransactionRepository = $orderTransactionRepository;
         $this->client = new Client();
     }
 
@@ -25,14 +33,12 @@ class BetterPaymentClient
         $requestParameters = array_merge($orderParameters, [
             'payment_type' => 'cc',
             'risk_check_approval' => '1',
-            'postback_url' => 'https://lightfulweb.free.beeceptor.com/bp-cc',
+            'postback_url' => EnvironmentHelper::getVariable('APP_URL').'/api/betterpayment/webhook',
             'success_url' => $transaction->getReturnUrl(),
-            'error_url' => 'http://localhost:8888/lightfulweb/shopware6/public/account/order/edit/'
-                .$transaction->getOrder()->getId()
-                .'?error-code=CHECKOUT__ASYNC_PAYMENT_PROCESS_INTERRUPTED', // TODO edit this temp url
+            'error_url' => EnvironmentHelper::getVariable('APP_URL').'/account/order/edit/' .$transaction->getOrder()->getId()
+                .'?error-code=CHECKOUT__ASYNC_PAYMENT_PROCESS_INTERRUPTED',
         ]);
 
-        // Do some API Call to your payment provider
         $headers = [
             'Authorization' => 'Basic REDACTED=',
             'Content-Type' => 'application/x-www-form-urlencoded'
@@ -47,9 +53,14 @@ class BetterPaymentClient
         // TODO log error codes and related messages to log file
         if ($response->getStatusCode() == 200) { // TODO almost all responses are 200 code, so check and handle them accordingly
             // store payment transaction_id to order transaction custom fields
-            $transaction->getOrderTransaction()->setCustomFields([
-                'betterPaymentTransactionID' => json_decode((string) $response->getBody())->transaction_id
-            ]);
+            $this->orderTransactionRepository->update([
+                [
+                    'id' => $transaction->getOrderTransaction()->getId(),
+                    'customFields' => [
+                        'better_payment_transaction_id' => json_decode((string) $response->getBody())->transaction_id
+                    ]
+                ]
+            ], Context::createDefaultContext());
 
             return json_decode((string) $response->getBody())->action_data->url;
         }
