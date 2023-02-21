75500d716a1fac5aac4bfa0b62856896aef00ee3
Hasan Hasanzade <h.hesen@hotmail.com>
2023-02-21 19:26:25 +0400

    added form for new refund and listing for refund history

--- src/Resources/app/administration/src/module/sw-order/view/sw-order-detail-base/index.js
+++ src/Resources/app/administration/src/module/sw-order/view/sw-order-detail-base/index.js
@@ -4,4 +4,19 @@ const { Component } = Shopware;
 
 Component.override('sw-order-detail-base', {
     template,
+
+    data() {
+        return {
+            refunds: []
+        };
+    },
+
+    created() {
+        this.refunds = [
+            {amount: '10.78 $', comment:'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet', date: '2023-01-12 10:45'},
+            {amount: '12.25 $', comment:'Lorem ipsum dolor sit amet', date: '2023-01-12 10:45'},
+            {amount: '5.30 $', comment:'Lorem ipsum dolor sit amet Lorem ipsum', date: '2023-01-12 10:45'}
+        ];
+    }
+
 });
