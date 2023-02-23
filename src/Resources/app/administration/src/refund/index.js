7a45fe3689415ca88338ef4f50b2de7da48dbd7d
Hasan Hasanzade <h.hesen@hotmail.com>
2023-02-23 21:29:25 +0400

    implemented create new refund feature for hard coded transaction_id

--- src/Resources/app/administration/src/refund/index.js
+++ src/Resources/app/administration/src/refund/index.js
@@ -1,21 +1,39 @@
 import template from './refund-card.html.twig';
 
-const { Component } = Shopware;
+const { Component, Mixin } = Shopware;
 
 Component.override('sw-order-detail-base', {
     template,
 
+    mixins: [
+        Mixin.getByName('notification'),
+    ],
+
     data() {
         return {
+            refund: {
+                amount: null,
+                comment: null,
+                execution_date: null,
+            },
             refunds: [],
+            processSuccess: false,
+            buttonDisabled: false,
         };
     },
 
+    created() {
+        this.getRefunds();
+    },
+
     methods: {
-        getRefundsData() {
-            const url = "https://devapi.betterpayment.de/rest/transactions/a2df9bfb-7af7-42d5-aff8-43950797ae5a/log";
+        getRefunds() {
+            // TODO: get api base url based on config
+            // TODO: get transaction_id from DB
+            const url = "https://devapi.betterpayment.de" + "/rest/transactions/b1c2c87c-5c4f-4f2a-ba44-6667e00fa6bd/log";
 
             const headers = new Headers();
+            // TODO get keys from config and generate authorisation
             headers.append("Authorization", "Basic NzBhYmQ1OTQwODQ3ODdhMzkyZTg6NGE2NmI5MWU5YjVjOTBjYTQ3YjA=");
 
             const requestOptions = {
@@ -23,16 +41,76 @@ Component.override('sw-order-detail-base', {
                 headers: headers,
             };
 
+            // TODO: consider status_code=error cases
+            // TODO: ? maybe show some errors to admin
             fetch(url, requestOptions)
                 .then(response => response.json())
                 .then(transactions => {
                     this.refunds = transactions.filter(transaction => transaction.type === 'refund');
                 })
-                .catch(error => console.log('error', error));
-        }
-    },
+                .catch(exception => {
+                    this.createNotificationError({
+                        title: 'Error fetching refunds',
+                        message: exception
+                    });
+                });
+        },
 
-    created() {
-        this.getRefundsData();
-    }
+        storeRefund() {
+            this.buttonDisabled = true;
+            // TODO: get api base url based on config
+            // TODO: get transaction_id from DB
+            const url = "https://devapi.betterpayment.de" + "/rest/refund";
+
+            const headers = new Headers();
+            // TODO get keys from config and generate authorisation
+            headers.append("Authorization", "Basic NzBhYmQ1OTQwODQ3ODdhMzkyZTg6NGE2NmI5MWU5YjVjOTBjYTQ3YjA=");
+            headers.append("Content-Type", "application/json");
+
+            const body = JSON.stringify({
+                "transaction_id": "b1c2c87c-5c4f-4f2a-ba44-6667e00fa6bd",
+                "amount": this.refund.amount,
+                "comment": this.refund.comment,
+                "execution_date": this.refund.execution_date
+            });
+
+            const requestOptions = {
+                method: 'POST',
+                headers: headers,
+                body: body
+            };
+
+            fetch(url, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    this.buttonDisabled = false;
+                    if (result.error_code === 0) {
+                        this.getRefunds();
+                        this.processSuccess = true;
+                        this.createNotificationSuccess({
+                            message: 'Refund saved successfully...'
+                        });
+                    }
+                    else {
+                        this.createNotificationError({
+                            message: result.error_message
+                        });
+                    }
+                })
+                .catch(exception => {
+                    this.createNotificationError({
+                        message: exception
+                    });
+                });
+        },
+
+        storeRefundFinished() {
+            this.refund.amount = null;
+            this.refund.comment = null;
+            this.refund.execution_date = null;
+            this.processSuccess = false;
+        }
+
+
+    },
 });
