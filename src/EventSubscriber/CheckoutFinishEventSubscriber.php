23de83a420e87a064e1c0000b5cb762e4357cc4c
Hasan Hasanzade <h.hesen@hotmail.com>
2023-02-26 16:31:19 +0400

    managed to show invoice instructions in CheckoutFinish page with reference number = order number

---
+++ src/EventSubscriber/CheckoutFinishEventSubscriber.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace BetterPayment\EventSubscriber;
+
+use BetterPayment\PaymentHandler\InvoiceB2BHandler;
+use BetterPayment\PaymentHandler\InvoiceHandler;
+use BetterPayment\Storefront\Struct\CheckoutData;
+use BetterPayment\Util\ConfigReader;
+use Shopware\Storefront\Page\Checkout\Finish\CheckoutFinishPageLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CheckoutFinishEventSubscriber implements EventSubscriberInterface
+{
+    private ConfigReader $configReader;
+
+    public function __construct(ConfigReader $configReader)
+    {
+        $this->configReader = $configReader;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            CheckoutFinishPageLoadedEvent::class => 'addInstructions',
+        ];
+    }
+
+    public function addInstructions(CheckoutFinishPageLoadedEvent $event): void
+    {
+        $page = $event->getPage();
+        $paymentMethod = $event->getSalesChannelContext()->getPaymentMethod();
+
+        if ($paymentMethod->getHandlerIdentifier() == InvoiceHandler::class && $this->configReader->getBool(ConfigReader::INVOICE_DISPLAY_INSTRUCTION)) {
+            $data = new CheckoutData();
+
+            $data->assign([
+                'template' => '@Storefront/betterpayment/invoice-instructions.html.twig',
+                'iban' => $this->configReader->getString(ConfigReader::INVOICE_IBAN),
+                'bic' => $this->configReader->getString(ConfigReader::INVOICE_BIC),
+                'orderID' => $page->getOrder()->getOrderNumber(),
+            ]);
+
+            $page->addExtension(CheckoutData::EXTENSION_NAME, $data);
+        }
+        elseif ($paymentMethod->getHandlerIdentifier() == InvoiceB2BHandler::class && $this->configReader->getBool(ConfigReader::INVOICE_B2B_DISPLAY_INSTRUCTION)) {
+            $data = new CheckoutData();
+
+            $data->assign([
+                'template' => '@Storefront/betterpayment/invoice-instructions.html.twig',
+                'iban' => $this->configReader->getString(ConfigReader::INVOICE_B2B_IBAN),
+                'bic' => $this->configReader->getString(ConfigReader::INVOICE_B2B_BIC),
+                'orderID' => $page->getOrder()->getOrderNumber(),
+            ]);
+
+            $page->addExtension(CheckoutData::EXTENSION_NAME, $data);
+        }
+    }
+}
