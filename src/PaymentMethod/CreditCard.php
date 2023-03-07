6b9d0a9823f7bf0b731d4eaa953f03d648ee47ef
Hasan Hasanzade <h.hesen@hotmail.com>
2023-03-07 18:08:12 +0400

    Rules for payment methods which has b2c and b2b options (#54)
    * added rules for payment methods which has b2c and b2b options
    
    * reimplemented Payment Methods with hard coded UUIDs for making processes easier to handle
    
    * restored B2B label in Payment Method names to avoid ambiguity in admin panel

--- src/PaymentMethod/CreditCard.php
+++ src/PaymentMethod/CreditCard.php
@@ -6,10 +6,13 @@ use BetterPayment\PaymentHandler\CreditCardHandler;
 
 class CreditCard extends PaymentMethod
 {
+    public const UUID = '818126dcd4e14c3ca658e935d032f73b';
     public const SHORTNAME = 'cc';
 
+    protected string $id = self::UUID;
     protected string $handler = CreditCardHandler::class;
     protected string $name = 'Credit Card';
+    protected string $shortname = self::SHORTNAME;
     protected string $description = '';
     protected string $icon = '';
     protected array $translations = [
