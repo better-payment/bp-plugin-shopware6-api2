b933a0a56768a6abe53eb783249712089ceac7fa
Hasan Hasanzade <h.hesen@hotmail.com>
2023-06-05 15:44:10 +0400

    added comments for the class and methods

--- src/EventSubscriber/OrderInvoiceDocumentCreatedEventSubscriber.php
+++ src/EventSubscriber/OrderInvoiceDocumentCreatedEventSubscriber.php
@@ -15,12 +15,24 @@ use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Class OrderInvoiceDocumentCreatedEventSubscriber
+ *
+ * This event subscriber listens to the 'document.written' event and performs actions when an invoice document is created.
+ */
 class OrderInvoiceDocumentCreatedEventSubscriber implements EventSubscriberInterface
 {
     private BetterPaymentClient $betterPaymentClient;
     private EntityRepositoryInterface $orderRepository;
     private ConfigReader $configReader;
 
+    /**
+     * OrderInvoiceDocumentCreatedEventSubscriber constructor.
+     *
+     * @param BetterPaymentClient $betterPaymentClient
+     * @param EntityRepositoryInterface $orderRepository
+     * @param ConfigReader $configReader
+     */
     public function __construct(BetterPaymentClient $betterPaymentClient, EntityRepositoryInterface $orderRepository, ConfigReader $configReader)
     {
         $this->betterPaymentClient = $betterPaymentClient;
@@ -28,6 +40,11 @@ class OrderInvoiceDocumentCreatedEventSubscriber implements EventSubscriberInter
         $this->configReader = $configReader;
     }
 
+    /**
+     * Returns the subscribed events and their corresponding methods.
+     *
+     * @return array The array of subscribed events and their methods.
+     */
     public static function getSubscribedEvents(): array
     {
         return [
@@ -35,12 +52,20 @@ class OrderInvoiceDocumentCreatedEventSubscriber implements EventSubscriberInter
         ];
     }
 
+    /**
+     * Performs following actions when an invoice document is created:
+     * 1. confirms whether this is really invoice type document to go on
+     * 2. fetches order from DB, and its corresponding successful(last) order transaction
+     * 3. checks whether this order transaction is capturable
+     * 4. sends capture request using BetterPaymentClient
+     *
+     * @param EntityWrittenEvent $event
+     */
     public function onInvoiceDocumentCreated(EntityWrittenEvent $event): void
     {
 		$payloads = $event->getPayloads();
 		foreach ($payloads as $payload) {
 			if (isset($payload['config']['name']) && $payload['config']['name'] == 'invoice') {
-				// Get order by orderId
 				$orderId = $payload['orderId'];
 				$criteria = new Criteria([$orderId]);
 				$criteria->addAssociation('transactions.paymentMethod');
@@ -56,21 +81,25 @@ class OrderInvoiceDocumentCreatedEventSubscriber implements EventSubscriberInter
 				if ($this->isCapturable($orderTransaction)) {
 					$invoiceId = $payload['config']['documentNumber'];
 
-					// Create capture request parameters
-					$parameters = [
+					$captureParameters = [
 	                    'transaction_id' => $orderTransaction->getCustomFields()['better_payment_transaction_id'],
 	                    'invoice_id' => $invoiceId,
 	                    'amount' => $order->getAmountTotal(),
 	                    'comment' => 'Captured using Shopware 6 plugin',
 	                ];
 
-					// Send capture request
-					$this->betterPaymentClient->capture($parameters);
+					$this->betterPaymentClient->capture($captureParameters);
 				}
 			}
 		}
     }
 
+    /**
+     * Checks if an order transaction is capturable based on the payment method and corresponding configuration flag.
+     *
+     * @param OrderTransactionEntity $orderTransaction
+     * @return bool Whether the order transaction is capturable or not.
+     */
 	public function isCapturable(OrderTransactionEntity $orderTransaction): bool
 	{
 		$paymentMethodShortname = $orderTransaction->getPaymentMethod()->getCustomFields()['shortname'];
